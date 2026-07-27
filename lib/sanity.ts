a2d07f265228b37aaa711c83b3963063d0439e2c
Dan Gahagan <dgahagan@gmail.com>
2026-07-26 23:30:25 -0400

    refactor(sanity): consolidate on one token-free client

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,12 +2,14 @@ import { createClient } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01';
+
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01',
-  useCdn: process.env.NODE_ENV === 'production',
-  token: process.env.SANITY_API_TOKEN,
+  apiVersion,
+  useCdn: false,
 });
 
 const builder = imageUrlBuilder(client);
--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'ChrisCakes',
 
-  projectId: '0fl6fs6u',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
   basePath: '/studio',
 
